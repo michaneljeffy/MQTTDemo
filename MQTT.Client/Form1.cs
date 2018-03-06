11fad1180403f17a8712728bfc3537b70d963943
zhangshuyu <zhangshuyu@yuantutech.com>
2018-03-06 15:01:16 +0800

    基于MQTT协议的demo

---
+++ MQTT.Client/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MQTTnet.Core.Client;
+using MQTTnet;
+using MQTTnet.Core;
+using MQTTnet.Core.Protocol;
+
+namespace MQTT.Client
+{
+    public partial class Form1 : Form
+    {
+        public static IMqttClient mqttClient = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+            try
+            {
+                var mqttFactory = new MqttFactory();
+                mqttClient = mqttFactory.CreateMqttClient() as MqttClient;
+
+                mqttClient.ApplicationMessageReceived += MqttClient_ApplicationMessageReceived;
+                mqttClient.Connected += MqttClient_Connected;
+                mqttClient.Disconnected += MqttClient_Disconnected;
+                //MqttClientTcpOptions
+                var clientOptions = new MqttClientOptions() { ClientId = Guid.NewGuid().ToString().Substring(5) };
+                mqttClient.ConnectAsync(clientOptions);
+            }
+
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+           
+        }
+
+        private void MqttClient_Disconnected(object sender, MqttClientDisconnectedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MqttClient_Connected(object sender, MqttClientConnectedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MqttClient_ApplicationMessageReceived(object sender, MQTTnet.Core.MqttApplicationMessageReceivedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void btnSubscrible_Click(object sender, EventArgs e)
+        {
+            mqttClient.SubscribeAsync(new List<TopicFilter> { new TopicFilter("test",MqttQualityOfServiceLevel.AtMostOnce)});
+        }
+
+        private void btnPublish_Click(object sender, EventArgs e)
+        {
+            var message = new MqttApplicationMessage() { Topic = "test", Payload = Encoding.UTF8.GetBytes("test"),Retain = false ,QualityOfServiceLevel = MqttQualityOfServiceLevel.AtMostOnce};
+            mqttClient.PublishAsync(message);
+        }
+    }
+}
---
+++ MQTT.Server/Program.cs
@@ -0,0 +1,52 @@
+﻿using MQTTnet;
+using MQTTnet.Core.Server;
+using System;
+using System.Threading;
+
+namespace MQTT.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            new Thread(StartMQTTServer).Start();
+        }
+
+        private static void StartMQTTServer()
+        {
+            try
+            {
+                var mqttFactory = new MqttFactory();
+                var server = mqttFactory.CreateMqttServer();
+                server.ApplicationMessageReceived += Server_ApplicationMessageReceived;
+                server.ClientConnected += Server_ClientConnected;
+                server.ClientDisconnected += Server_ClientDisconnected;
+                var mqOptions = new MqttServerOptions();
+                 server.StartAsync(mqOptions);
+                Console.WriteLine("服务成功开启");
+                Console.ReadLine();
+            }
+
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }        
+        }
+
+        private static void Server_ClientDisconnected(object sender, MqttClientDisconnectedEventArgs e)
+        {
+            Console.WriteLine("client {0} connected",e.Client.ClientId);
+        }
+
+        private static void Server_ClientConnected(object sender, MqttClientConnectedEventArgs e)
+        {
+            Console.WriteLine("client {0} disconnected", e.Client.ClientId);
+        }
+
+        private static void Server_ApplicationMessageReceived(object sender, MQTTnet.Core.MqttApplicationMessageReceivedEventArgs e)
+        {
+            Console.WriteLine("client {0} send message {1}",e.ClientId, e.ApplicationMessage );
+        }
+    }
+}
